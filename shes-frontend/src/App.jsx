978f045a7a9e06cbbc8aa4627d5cd3260aef95fc
amerhatem <amerhatem01@gmail.com>
2025-05-26 23:14:15 +0300

    upload patient dash

--- shes-frontend/src/App.jsx
+++ shes-frontend/src/App.jsx
@@ -3,10 +3,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import ForgotPassword from './pages/ForgotPassword';
 import Register from './pages/Register';
+
 import PatientHome from './pages/patient/PatientHome';
 import Profile from './pages/patient/Profile';
 import Doctors from './pages/patient/Doctors';
 import MyAppointments from './pages/patient/MyAppointments';
+import UploadReport from './pages/patient/UploadReport';
+
+
 import DoctorHome from './pages/doctor/DoctorHome';
 import AdminHome from './pages/admin/AdminHome';
 import SupervisorHome from './pages/supervisor/SupervisorHome';
@@ -21,10 +25,14 @@ export default function App() {
       <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/register" element={<Register />} />
+
       <Route path="/patient/home" element={<PatientHome />} />
        <Route path="/patient/Profile" element={<Profile />} />
        <Route path="/patient/Doctors" element={<Doctors />} />
          <Route path="/patient/appointments" element={<MyAppointments />} />
+               <Route path="/patient/upload-report" element={<UploadReport />} />
+
+         
       <Route path="/doctor/home" element={<DoctorHome />} />
       <Route path="/admin/home" element={<AdminHome />} />
       <Route path="/supervisor/home" element={<SupervisorHome />} />
